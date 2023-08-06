d8e91604c5beeb17bb198629369810cdb722b17f
Danindu Dilmina <ddleewanage@nsbm.students.ac.lk>
2023-08-07 03:38:54 +0530

    Done until about

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
+import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
 
-import React from 'react'
 
 const App = () => {
   return (
     <>
       <Header />
+      <main className='main'>
+        <Home />
+        <About />
+      </main>
     </>
   )
 }
