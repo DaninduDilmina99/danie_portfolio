df85affe0d597b008eaae61e20e00c275d901245
Danindu Dilmina <ddleewanage@students.nsbm.ac.lk>
2023-08-30 18:14:32 +0530

    email API done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
-import Testimonials from './components/testimonials/Testimonials';
+// import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
 
 
 
@@ -20,7 +21,8 @@ const App = () => {
         <Skills />
         <Services />
         <Qualification/>
-        <Testimonials/>
+        {/* <Testimonials/> */}
+        <Contact/>
       </main>
     </>
   )
